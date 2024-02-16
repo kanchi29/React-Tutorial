e1c7fe870961451cd8e199dbfae41df058fb2cfc
kanchi29 <kanchisri29@gmail.com>
2024-02-16 18:14:09 +0530

    Convert text to uppercase using state hooks.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
     <Navbar title="Textutils" about="About Textutils" />
+    <div className="container my-3"><TextForm heading="Enter the text to be analysed"/>
+    </div>
     </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        //console.log("UpperCase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleOnChange = (event)=>{
+       // console.log("On Change");
+        setText(event.target.value);
+    }
+    const[text, setText] = useState('Enter text here');
+    //setText("Updated value of text is written here");
+  return (
+ <div>
+    <h1>{props.heading}</h1>
+    <div className="mb-3">
+        <textarea className="form-control" id="myBox" rows="8" onChange={handleOnChange} value={text}></textarea>
+    </div>
+    <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+ </div>
+  )
+}
