75d8df4c0ce0abf350a460e8ffce29f894e128c1
kanchi29 <kanchisri29@gmail.com>
2024-02-17 23:10:28 +0530

    Dark Mode in navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 // import logo from './logo.svg';
 
-import About from './components/About';
+// import About from './components/About';
+
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');
   return (
     <>
-    <Navbar title="Textutils" about="About Textutils" />
+    <Navbar title="Textutils" about="About Textutils" mode={mode}/>
     <div className="container my-3">
-      {/* <TextForm heading="Enter the text to be analysed"/> */}
-      <About/>
+      <TextForm heading="Enter the text to be analysed"/>
+      {/* <About/> */}
     </div>
     
     </>
